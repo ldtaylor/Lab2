d2d9caaa882d5ce041a1399f04fadbe8b0f6393c
ldtaylor <lut11@pitt.edu>
2016-01-21 16:51:37 -0500

    Fixed changes

--- Lab2.java
+++ Lab2.java
@@ -16,31 +16,31 @@ public class Lab2 {
 	// Assume that the original man is also going to St. Ives, so
 	// the boolean variable manGoingToStIves should be true.
 	
-	boolean manGoingToStIves = !true;
+	boolean manGoingToStIves = true;
 
-	// short numWives = 8;
+	int numWives = 8;
 
-	// int numSacksPerWife = (double) 7;
+	int numSacksPerWife = 7;
 	
-	// int numCatsPerSack = 7.0;
+	int numCatsPerSack = 7;
 
-	// int numKitsPerCat = (long) 7;
+	int numKitsPerCat = 7;
 
-	// byte total;
+	int total;
 
 	// If the original man is going to St. Ives, then all are going
 	// the narrator, original man, kitten, cats, sacks, wives.
 	// If the original man is going AWAY from St. Ives, then the only
 	// person going is the narrator
 	
-	int total;
 
 	if (manGoingToStIves) {
+		
 	    // 2. Remember, if the original man is going to St. Ives, then
 	    // all of the wives, sacks, cats, and kittens are as well, along
 	    // with the narrator.
 	    // Calculate the number going to St. Ives
-	    total = -1;
+	    total = numWives + numSacksPerWife + numCatsPerSack + numKitsPerCat;
 	} else {
 	    // Otherwise, only the narrator is going!
 	    total = 1;
@@ -50,6 +50,8 @@ public class Lab2 {
 	// "Number going to St. Ives is " and then, on the same line,
 	// the number going to St. Ives (stored in the _total_ variable).
 	// Add a statement or statements that will do this.
+	
+	System.out.print("Number going to St. Ives is " + total);
 	
     }
     
